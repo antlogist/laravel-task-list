8ad9d0286eb7ad9359a6d5a4b1119d7b18e40adf
antlogist <antlogist@yandex.ru>
2023-10-04 14:19:26 +0300

    Added error message

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -8,11 +8,19 @@
     <div>
         <label for="title">Title</label>
         <input type="text" name="title" id="title" value="{{ isset($task) ? $task->title : old('title') }}">
+        
+        @error('title')
+            <p>{{ $message }}</p>
+        @enderror
     </div>
 
     <div>
         <label for="description">Description</label>
         <textarea name="description" id="description" cols="30" rows="10">{{ isset($task) ? $task->description : old('description') }}</textarea>
+
+        @error('description')
+            <p>{{ $message }}</p>
+        @enderror
     </div>
 
     <div>
